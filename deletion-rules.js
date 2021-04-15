33e17fbf9e237bc8aec6c7bf220d1fb2ef66189e
Esther Weidauer <esther@selfawaresoup.com>
2021-04-15 20:55:03 +0200

    update rules

--- deletion-rules.js
+++ deletion-rules.js
@@ -1,10 +1,10 @@
 module.exports = {
   keepTweets: [
-    '1103961200688328704',
+    '1188883571555143683',
   ],
   earliestDate: 'Mon Jan 1 00:00:00 +0000 2019',
-  minimumLikes: 15,
-  minimumRetweets: 15,
+  minimumLikes: 30,
+  minimumRetweets: 20,
   maxTweetAge: 60,
   maxRetweetAge: 30
 }

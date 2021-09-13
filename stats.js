b4a1e61de7e0302abc3a0b4e48ce8ea3c17e631a
Esther Weidauer <esther@selfawaresoup.com>
2021-09-14 00:46:31 +0200

    stats tool

---
+++ stats.js
@@ -0,0 +1,19 @@
+const { client } = require('./lib')
+
+const blocks = async (cursor = -1) => {
+  let ids = []
+
+  do {
+    const res = await client.get(`blocks/ids`, {include_entities: false, stringify_ids: true, cursor})
+    cursor =  res.next_cursor
+    ids = ids.concat(res.ids)
+  } while (cursor != 0)
+  
+  return ids
+}
+
+blocks().then(ids => {
+  console.log(`BLOCKED: ${ids. length}`)
+})
+
+
